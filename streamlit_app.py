d33787f64fdf960761ac71238400d90a3a0fde66
MEHUL GUPTA <31255225+mehulgupta2016154@users.noreply.github.com>
2023-10-31 22:18:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -155,7 +155,7 @@ with st.expander('read my latest blogs below'):
 st.subheader('Youtube ▶️')
 st.markdown("""<a href={}> access channel here</a>""".format(info['youtube_url']),unsafe_allow_html=True)
 page1,page2 = requests.get(info['youtube_url']), requests.get(info['youtube_about'])
-subs = re.findall('(\d+) subscribers',page1.text)[0]
+subs = re.findall('(\d+\.\d+[kK]?) subscribers',page1.text)[0]
 videos = re.findall( r'"videosCountText".*?"text":"(\d+)"',page1.text)[0]
 views = re.findall( r'"viewCountText".*?"simpleText":"(\d+)',page2.text.replace(',',''))[0]
 
