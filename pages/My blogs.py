057fb0aea119fbafd68dbcaa994142cd6cfb07dd
MEHUL GUPTA <31255225+mehulgupta2016154@users.noreply.github.com>
2024-06-25 15:16:41 +0530

    Update My blogs.py

--- pages/My blogs.py
+++ pages/My blogs.py
@@ -7,7 +7,7 @@ name="LangChain in your Pocket: Beginner's Guide to Building Generative AI Appli
 url="https://medium.com/data-science-in-your-pocket/my-first-book-langchain-in-your-pocket-is-out-9a1f156c0f7b"
 st.markdown("""<a href={}><b><u>{}</b></u></a>""".format(url,name),unsafe_allow_html=True)
 
-st.error('My AI Podcast, AIQ is out now', icon="📕")
+st.error('My AI Podcast, AIQ is out now', icon="🎤")
 name="AIQ : Artificial Intelligence Quotient"
 url="https://www.youtube.com/watch?v=Qj_hlIRZiJg&list=PLnH2pfPCPZsIH5TUPuyu2fVbJDYjK1RVw"
 st.markdown("""<a href={}><b><u>{}</b></u></a>""".format(url,name),unsafe_allow_html=True)
